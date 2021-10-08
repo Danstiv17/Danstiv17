39f359f645263e2f6411bac6a33037b16f390da1
Daniel Zuleta <89160344+Danstiv17@users.noreply.github.com>
2021-10-08 16:46:47 -0500

    Delete views directory

--- views/modules/errorintentos.php
+++
@@ -1,7 +0,0 @@
-<div>
-	<h1>Estimado comentario</h1>
-</div>
-<div>
-	<p> Estimado comentario, Usted acaba de ingresar tres veces con una clave errada</p>
-	<p>Por favor pangase en contacto con el administrador del sitio</p>
-</div>
