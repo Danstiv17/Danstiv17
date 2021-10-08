33507375d7e48e924e633413da43f028ebd5f7b8
Daniel Zuleta <89160344+Danstiv17@users.noreply.github.com>
2021-10-08 16:46:01 -0500

    Delete config directory

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	define('URL', 'http://localhost/tocaimapp/');
-	define('HOST', 'localhost');
-	define('BD', 'tocaimapp');
-	define('PASSWORD', '');
-	define('CHARSET', 'utf8mb4');
-	define('USER', 'root');
-?>
